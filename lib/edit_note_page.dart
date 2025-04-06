35acbd16641d9d54a9ab7f8793cadcc1dadf9802
dizon-kevin <ashleynicolegutierrez7@gmail.com>
2025-04-06 20:06:45 +0800

    commit by kevin

--- lib/edit_note_page.dart
+++ lib/edit_note_page.dart
@@ -27,3 +27,64 @@ class _EditNotePageState extends State<EditNotePage> {
     _titleController = TextEditingController(text: widget.note['title']);
     _tagController = TextEditingController(text: widget.note['tag']);
   }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _tagController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      backgroundColor: Colors.white, // Set background to white
+      navigationBar: CupertinoNavigationBar(
+        middle: Text(
+          'Edit Note',
+          style: TextStyle(color: Colors.black), // Set text to black
+        ),
+        previousPageTitle: 'Notes', // Set back button text
+        trailing: CupertinoButton(
+          padding: EdgeInsets.zero,
+          child: Text('Save'),
+          onPressed: () {
+            widget.updateNote(
+              widget.index,
+              _titleController.text,
+              _tagController.text,
+            );
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              CupertinoTextField(
+                controller: _titleController,
+                placeholder: 'Title',
+                style: TextStyle(color: Colors.black),
+                decoration: BoxDecoration(color: Colors.white),
+              ),
+              SizedBox(height: 10),
+              CupertinoTextField(
+                controller: _tagController,
+                placeholder: 'Tag (e.g., #work, #travel)',
+                style: TextStyle(color: Colors.black),
+                decoration: BoxDecoration(color: Colors.white),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Last edited: ${DateFormat('MMM d, y, h:mm a').format(widget.note['date'])}',
+                style: TextStyle(color: Colors.grey, fontSize: 14),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
